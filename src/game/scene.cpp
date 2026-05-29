c1ee90c48c8c9ae18882315bcaa6fedb53d6179d
alex <alexcox4132@gmail.com>
2026-05-28 21:17:53 -0400

    testink

--- src/game/scene.cpp
+++ src/game/scene.cpp
@@ -21,7 +21,7 @@ namespace scene {
 		chess = renderer::load_model("assets/models/chess/chess.gltf");
 
 		renderer::set_sun({ 0.38f, 1.0f, 0.41f }, { 1.0f, 1.0f, 1.0f }, 0.0f);
-		env_cubemap = renderer::load_cubemap("field", 0.0f);
+		env_cubemap = renderer::load_cubemap("whitestudio", 3.0f);
 		if (env_cubemap != renderer::INVALID_CUBEMAP) {
 			renderer::set_environment_cubemap(env_cubemap);
 		}
@@ -51,9 +51,9 @@ namespace scene {
 		renderer::submit_model(chess);
 
 		// test point lights
-		renderer::submit_light({ 4.0f, 4.0f, 0.0f },  { 1.0f, 0.3f, 0.1f }, 10.0f, 30.0f);
-		renderer::submit_light({-4.0f, 4.0f, 0.0f },  { 0.1f, 0.3f, 1.0f }, 10.0f, 30.0f);
-		renderer::submit_light({ 0.0f, 5.0f, 8.0f },  { 0.2f, 1.0f, 0.2f }, 10.0f, 30.0f);
+		//renderer::submit_light({ 4.0f, 4.0f, 0.0f },  { 1.0f, 0.3f, 0.1f }, 10.0f, 30.0f);
+		//renderer::submit_light({-4.0f, 4.0f, 0.0f },  { 0.1f, 0.3f, 1.0f }, 10.0f, 30.0f);
+		//renderer::submit_light({ 0.0f, 5.0f, 8.0f },  { 0.2f, 1.0f, 0.2f }, 10.0f, 30.0f);
 
 		// --- FPS HUD ---
 		fps_accum_time   += dt;
